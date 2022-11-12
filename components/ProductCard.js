3fdc7f10c242c570387ad3818e8c3d378c61f263
Ikenna <iykee0333@gmail.com>
2022-11-12 13:58:02 +0000

    product card

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Link from "next/link";
 import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import { appear } from "../utils/MotionVariants";
 import { urlFor } from "../lib/sanity";
 import { HiOutlineShoppingBag } from "react-icons/hi";
 import { AiOutlineHeart } from "react-icons/ai";
@@ -8,17 +10,32 @@ import { AiOutlineHeart } from "react-icons/ai";
 const ProductCard = ({ product }) => {
 	return (
 		<Box
+			role="group"
 			as={motion.div}
-			initial={{ opacity: 0, transition: { duration: 1 } }}
-			whileInView={{ opacity: 1 }}
+			variants={appear}
+			initial={"initial"}
+			whileInView={"animate"}
 			shadow="md">
 			<Box position="relative">
+				{product?.onSale && (
+					<Box
+						bg="orange.400"
+						color="white"
+						px={6}
+						py={2}
+						position="absolute"
+						top="0"
+						left="0">
+						SALE
+					</Box>
+				)}
 				<Image
 					src={urlFor(product.image[0]).url()}
 					alt={product.title}
 					width="full"
-					minHeight="14rem"
+					minHeight={{ base: "14rem", md: "16rem" }}
 					objectFit="cover"
+					_groupHover={{ transform: "scale(1.05)" }}
 				/>
 				<Flex
 					direction="column"
@@ -47,23 +64,31 @@ const ProductCard = ({ product }) => {
 				</Flex>
 			</Box>
 			<Box mb={2}>
-				<Button
-					w="full"
-					py={6}
-					fontSize="1.1rem"
-					rounded="none"
-					bgColor="blue.400"
-					transition="0.5s"
-					_hover={{ bgColor: "black", color: "white" }}>
-					VIEW
-				</Button>
+				<Link href={`product/${product.slug.current}`}>
+					<Button
+						w="full"
+						py={6}
+						fontSize="1.1rem"
+						rounded="none"
+						bgColor="blue.400"
+						transition="0.5s"
+						_hover={{ bgColor: "black", color: "white" }}>
+						VIEW
+					</Button>
+				</Link>
 			</Box>
 			<Box textAlign="center" pb={2}>
 				<Text color="gray.400">{product.vendor.toUpperCase()}</Text>
 				<Text fontSize="1.1rem">{product.title}</Text>
 				<Text fontWeight={600} fontSize="1.1rem" color="orange.400">
-					N{product.price.toFixed(2)}
+					{"₦"}
+					{product.price.toFixed(2)}
 				</Text>
+				{product?.onSale && (
+					<Text as="s" color="gray.400">
+						N{product?.oldPrice?.toFixed(2)}
+					</Text>
+				)}
 			</Box>
 		</Box>
 	);
