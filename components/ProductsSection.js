d9880887780fd09414f7ce9677cc22e9a30defc8
Ikenna <iykee0333@gmail.com>
2022-11-12 14:56:43 +0000

    added extralink prop to product section component

--- components/ProductsSection.js
+++ components/ProductsSection.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { Box, Container, Grid, Heading } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { Box, Button, Container, Flex, Grid, Heading } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import ProductCard from "./ProductCard";
 
-const ProductsSection = ({ title, products }) => {
+const ProductsSection = ({ title, products, extraLink }) => {
 	return (
 		<Box mt={12}>
-			<Container maxWidth="container.lg">
-				<Heading textAlign="center" mb={5}>
+			<Container maxWidth="container.xl">
+				<Heading
+					as={motion.h2}
+					initial={{ opacity: 0, transition: { duration: 1 } }}
+					whileInView={{ opacity: 1 }}
+					textAlign="center"
+					mb={5}>
 					{title}
 				</Heading>
 				<Grid
@@ -17,6 +23,20 @@ const ProductsSection = ({ title, products }) => {
 						<ProductCard key={product._id} product={product} />
 					))}
 				</Grid>
+				{extraLink && (
+					<Flex justify="center" mt={3}>
+						<NextLink href={extraLink}>
+							<Button
+								bg="blackAlpha.800"
+								color="white"
+								_hover={{ bg: "blackAlpha.700" }}
+								rounded="none"
+								px={8}>
+								See More
+							</Button>
+						</NextLink>
+					</Flex>
+				)}
 			</Container>
 		</Box>
 	);
