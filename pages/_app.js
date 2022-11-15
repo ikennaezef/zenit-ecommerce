1bce4225dcbfd10143134694c0ad0ba02182becc
Ikenna <iykee0333@gmail.com>
2022-11-15 01:47:34 +0000

    setup context

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Layout } from "../components";
+import { ContextWrapper } from "../context";
+import { Toaster } from "react-hot-toast";
 
 const fonts = {
 	body: "Work Sans, sans-serif",
@@ -12,9 +14,12 @@ const theme = extendTheme({ fonts });
 function MyApp({ Component, pageProps }) {
 	return (
 		<ChakraProvider theme={theme}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<ContextWrapper>
+				<Layout>
+					<Toaster />
+					<Component {...pageProps} />
+				</Layout>
+			</ContextWrapper>
 		</ChakraProvider>
 	);
 }
