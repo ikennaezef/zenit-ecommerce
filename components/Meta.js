71881a13622dfb31294a0209e43c480988f258e9
Ikenna <iykee0333@gmail.com>
2022-11-21 22:02:20 +0000

    meta component update

--- components/Meta.js
+++ components/Meta.js
@@ -8,6 +8,29 @@ const Meta = ({ title, description }) => {
 			<title>{title}</title>
 
 			<meta name="application-name" content="Zenit Stores" />
+
+			<link rel="manifest" href="/site.webmanifest" />
+
+			<link rel="shortcut icon" href="/favicon.ico" />
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon-16x16.png"
+			/>
+
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-touch-icon.png"
+			/>
+			<link rel="mask-icon" href="/favicon-16x16.png" color="grey"></link>
 		</Head>
 	);
 };
@@ -17,5 +40,5 @@ export default Meta;
 Meta.defaultProps = {
 	title: "Zenit Stores",
 	description:
-		"Zenit Stores is your one stop ecommerce choice for elctronics, home accessories, fashion apparel and much more.",
+		"Zenit Stores is your one stop ecommerce choice for electronics, home accessories, fashion apparel and much more.",
 };
