1493a3a608fe4d62890a839f91a6b7206115608a
Ikenna <iykee0333@gmail.com>
2022-11-23 00:31:11 +0000

    categories page

---
+++ pages/categories.js
@@ -0,0 +1,102 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import { ProductsSection } from "../components";
+import { sanityClient } from "../lib/sanity";
+
+const Categories = ({
+	fashionProducts,
+	homeProducts,
+	accessoriesProducts,
+	electronicsProducts,
+}) => {
+	return (
+		<Box py={8}>
+			<ProductsSection
+				title="Fashion"
+				products={fashionProducts}
+				extraLink="/category/fashion"
+			/>
+			<ProductsSection
+				title="Home"
+				products={homeProducts}
+				extraLink="/category/home"
+			/>
+			<ProductsSection
+				title="Accessories"
+				products={accessoriesProducts}
+				extraLink="/category/accessories"
+			/>
+			<ProductsSection
+				title="Electronics"
+				products={electronicsProducts}
+				extraLink="/category/electronics"
+			/>
+		</Box>
+	);
+};
+
+export default Categories;
+
+export const getServerSideProps = async () => {
+	const fashionQuery = `*[_type == 'product' && 'Fashion' in categories[]->title][0...4]{
+		_id,
+		'category': categories[0] -> title,
+		'vendor': vendor -> title,
+		slug,
+		title,
+		price,
+		image,
+		onSale,
+		oldPrice
+	}`;
+
+	const homeQuery = `*[_type == 'product' && 'Home' in categories[]->title][0...4]{
+		_id,
+		'category': categories[0] -> title,
+		'vendor': vendor -> title,
+		slug,
+		title,
+		price,
+		image,
+		onSale,
+		oldPrice
+	}`;
+
+	const accessoriesQuery = `*[_type == 'product' && 'Accessories' in categories[]->title][0...4]{
+		_id,
+		'category': categories[0] -> title,
+		'vendor': vendor -> title,
+		slug,
+		title,
+		price,
+		image,
+		onSale,
+		oldPrice
+	}`;
+
+	const electronicsQuery = `*[_type == 'product' && 'Electronics' in categories[]->title][0...4]{
+		_id,
+		'category': categories[0] -> title,
+		'vendor': vendor -> title,
+		slug,
+		title,
+		price,
+		image,
+		onSale,
+		oldPrice
+	}`;
+
+	const fashionProducts = await sanityClient.fetch(fashionQuery);
+	const homeProducts = await sanityClient.fetch(homeQuery);
+	const accessoriesProducts = await sanityClient.fetch(accessoriesQuery);
+	const electronicsProducts = await sanityClient.fetch(electronicsQuery);
+
+	return {
+		props: {
+			fashionProducts,
+			homeProducts,
+			accessoriesProducts,
+			electronicsProducts,
+		},
+	};
+};
