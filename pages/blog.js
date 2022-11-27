c97a6bf8f43077d7dbac5ca053b43ec1ab2a0141
Ikenna <iykee0333@gmail.com>
2022-11-27 19:44:04 +0000

    blog page

---
+++ pages/blog.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Container, Text } from "@chakra-ui/react";
+
+const blog = () => {
+	return (
+		<Box>
+			<Container maxWidth="container.xl" py={8}>
+				<Text>Zenit Blog Coming soon...</Text>
+			</Container>
+		</Box>
+	);
+};
+
+export default blog;
